ad79c27600f5d44d7ffbb65ab28401728f978eb2
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-08-15 04:45:19 +0530

    jQuery fullcalender example

---
+++ MvcEventCalendar/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using MvcEventCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcEventCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            List<Event> events = new List<Event> {
+                new Event
+                {
+                    Name="Rakesh",
+                    StartDate="2017-08-15",
+                    EndDate="2017-08-17",
+                    ThemeColor="red",
+                    IsFullDay=true
+                },
+                new Event
+                {
+                    Name="Vikash",
+                    StartDate="2017-08-10",
+                    EndDate="2017-08-11",
+                    ThemeColor="green",
+                    IsFullDay=true
+                }
+
+            };
+            return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
---
+++ MvcEventCalendar/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcEventCalendar.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string ThemeColor { get; set; }
+        public bool IsFullDay { get; set; }
+    }
+}
